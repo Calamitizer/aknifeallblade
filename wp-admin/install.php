e9d56d0e7537141b5f7fdb13702cc2a3379360e2
Calamitizer <jaruble@ncsu.edu>
2017-11-17 17:52:31 -0500

    Tweak admin install header

--- wp-admin/install.php
+++ wp-admin/install.php
@@ -1,8 +1,9 @@
 <?php // install.php replacement page: https://perishablepress.com/press/2009/05/05/important-security-fix-for-wordpress/ ?>
-<?php header("HTTP/1.1 503 Service Temporarily Unavailable"); ?>
-<?php header("Status 503 Service Temporarily Unavailable"); ?>
-<?php header("Retry-After 3600"); // 60 minutes ?>
-<?php mail("jaruble@ncsu.edu", "Database Error", "There is a problem with teh database!"); ?>
+<?php
+    header("Status 503 Service Temporarily Unavailable");
+    header("Retry-After 3600"); // 60 minutes
+?>
+<?php mail("jaruble@ncsu.edu", "Database Error", "The database could not be accessed."); ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xml:lang="en" xmlns="http://www.w3.org/1999/xhtml" lang="en">
     <head>
